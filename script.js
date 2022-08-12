ca64faf999acf0098072657b6dff2f1a8b943c14
anggaSaputra5 <111058324+anggaSaputra5@users.noreply.github.com>
2022-08-12 10:54:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let body = document.querySelector("body");
-let light = document.querySelector("#light");
+const body = document.querySelector("body");
+const light = document.querySelector("#light");
+const light2 = document.querySelector("#light2");
+const menu = document.querySelector(".menu");
+const sidebar = document.querySelector("#sidebar");
+let nilai = 0;
 
 light.addEventListener("click", function(){
     body.classList.toggle("light");
+});
+light2.addEventListener("click", function(){
+    body.classList.toggle("light");
+});
+
+menu.addEventListener("click", function(){
+    if( nilai == 0){
+        sidebar.style.left = "0%";
+        sidebar.style.transition = "linear 0.3s all";
+        nilai = 1;
+    }
+    else{
+        sidebar.style.left = "-50%";
+        nilai = 0;
+    }
 });
